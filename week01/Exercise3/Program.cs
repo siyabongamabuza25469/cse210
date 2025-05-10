e498a82461dbd2151381d9c6429afc9154c80bfa
siyabongamabuza25469 <mshengu25469@gmail.com>
2025-05-10 14:44:29 +0200

    exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -6,17 +6,14 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
         
-        // For Part 1 and 2, the user characterized the number
         Console.Write("What is the magic number?");
         int magicNumber = int.Parse(Console.ReadLine());
 
-        // For Part 3, a random number is used
         Random randomGenerator = new Random();
         int magic = randomGenerator.Next(1, 101);
 
         int quess = -1;
 
-        //Used of a do-while loop
         while (quess != magicNumber)
         {
             Console.Write("What is your quess?");
