66f51d92f8253bec557204c3ada89bb36e61f938
siyabongamabuza25469 <mshengu25469@gmail.com>
2025-05-10 14:40:30 +0200

    exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Hello World! This is the Exercise 2 Project.");
 
         Console.Write("What is your grade percentage?");
         string answer = Console.ReadLine();
